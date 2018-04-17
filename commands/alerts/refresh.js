23a3e905652ffe495258cc6102b82bbd07a7874b
Wokki <iiro.seppanen@student.hamk.fi>
2018-04-17 04:12:46 +0300

    Command to refresh live statuses

---
+++ commands/alerts/refresh.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord.js-commando');
+const winston = require('winston');
+const _ = require('lodash');
+const request = require('request');
+
+const { GOOGLE_API_KEY } = require('../../config.json');
+const streams = require('../../util/streams.js');
+
+module.exports = class RefreshCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'refresh',
+            group: 'alerts',
+            memberName: 'refresh',
+            description: 'Refresh alerts immediately',
+            details: 'Does not overwrite the current timer of stream check. Still occurs every 2 minutes.',
+            examples: [`${client.commandPrefix}refresh`],
+            guildOnly: true,
+            clientPermissions: ['SEND_MESSAGES']
+        });
+    }
+
+    run(msg) {
+        streams.checkTwitch(this.client);
+        //streams.checkYoutube(this.client);
+        msg.channel.send(`Streamer statuses refreshed`);
+    }
+}
